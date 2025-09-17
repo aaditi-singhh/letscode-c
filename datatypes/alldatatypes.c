81c1c105f600ec2af61d1c4f6c4a389df487cc7e
aaditi-singhh <aditisingh6841@gmail.com>
2025-09-18 01:00:29 +0530

    Add files via upload

---
+++ datatypes/alldatatypes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+    int main(){
+        
+    int a;
+    float b;
+    char c;
+    
+    printf("Enter an integer ");
+    scanf("%d",&a);
+   
+    printf("Enter a decimal number ");
+    scanf("%f",&b);
+    
+    //The %c" format string tells scanf to skip any whitespace(spaces, tabs, newlines) before reading the character.
+    //Without that space, scanf picks up the leftover newline instead of waiting for actual input.
+    printf("Enter a char :");
+    scanf(" %c",&c);  
+    
+    printf("The entered integer is: %d \n",a);
+    printf("The entered float is: %f \n",b);
+    printf("The entered char is : %c",c);
+
+    return 0;
+}
