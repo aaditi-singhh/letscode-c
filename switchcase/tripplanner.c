234353b2a7b8baad3b8e2e41f8c8b429429d23f4
aaditi-singhh <aditisingh6841@gmail.com>
2025-09-21 21:01:18 +0530

    Update tripplanner.c

--- switchcase/tripplanner.c
+++ switchcase/tripplanner.c
@@ -2,6 +2,7 @@
 
 int main(){
     int details;
+        int total_transport = 0, total_accom = 0, total_food = 0, expected_price = 0;
     
     //name
     char name[100] = " ";
@@ -10,6 +11,8 @@ int main(){
     
     //destination
     char destination[100] = " ";
+    printf("enter destination \n");
+    scanf("%s",destination);
     
     int km;
     char transportation;
@@ -33,8 +36,7 @@ int main(){
     
     switch(details){
     case 1:
-    printf("enter destination \n");
-    scanf("%s",destination);
+    
     printf("enter distance from source \n");
     scanf("%d", &km);
     printf("choose transportation \n");
@@ -47,51 +49,57 @@ int main(){
     
         switch(transportation){
             
-            case 'T':
+            case 'T' :case 't' :
             printf("enter train fare \n");
             scanf("%d", &trainfare);
+            total_transport = trainfare;
             break;
             
-            case 'B':
+            case 'B':case 'b':
             printf("enter bus fare \n");
             scanf("%d", &busfare);
+            total_transport = busfare;
             break;
             
-            case 'F':
+            case 'F':case 'f':
             printf("enter flight fare \n");
             scanf("%d",&flightfare);
+            total_transport = flightfare;
             break;
             
-            case 'M':
+            case 'M':case 'm':
             printf("write details about motorbike fare \n");
             printf("enter petrol price \n");
             scanf("%d",&petrolprice);
             printf("enter fasttag price \n");
             scanf("%d", &fasttagprice);
-            motorbikefare=petrolprice+fasttagprice;
+            motorbikefare=km*petrolprice+fasttagprice;
             printf("motorbike fare: %d",motorbikefare);
+            total_transport = motorbikefare;
             break;
             
             
-            case 'C':
+            case 'C': case 'c':
             printf("write details about car fare \n");
             printf("enter petrol price \n");
             scanf("%d",&petrolprice_c);
             printf("enter fasttag price \n");
             scanf("%d", &fasttagprice_c);
-            carfare=petrolprice_c+fasttagprice_c;
+            carfare=km*petrolprice_c+fasttagprice_c;
             printf("car fare: %d",carfare);
+            total_transport = carfare;
             break;
             
         }
-        break;
+        
         int days;
         int accom;
         case 2:
         
+      
+        printf("details about accomodation \n");
         printf("how many days you are staying \n");
         scanf("%d",&days);
-        printf("details about accomodation \n");
         printf("1.Home stay \n");
         printf("2.hotels \n");
         printf("3.guest house \n");
@@ -104,7 +112,8 @@ int main(){
                 printf("enter room price per day \n");
                 scanf("%d",&homestayp);
                 homestayprice = days* homestayp;
-                printf("home stay price: %d", homestayprice);
+                printf("home stay price: %d \n", homestayprice);
+                total_accom = homestayprice;
                 break;
                 
                 int hotelp;
@@ -113,7 +122,8 @@ int main(){
                 printf("enter room price per day \n");
                 scanf("%d",&hotelp);
                 hotelprice = days* hotelp;
-                printf("hotel price: %d", hotelprice);
+                printf("hotel price: %d \n", hotelprice);
+                total_accom = hotelprice;
                 break;
                 
                 int guesthousep;
@@ -122,17 +132,69 @@ int main(){
                 printf("enter room price per day \n");
                 scanf("%d",&guesthousep);
                 guesthouseprice = days* guesthousep;
-                printf("guesthouse price: %d", guesthouseprice);
+                printf("guesthouse price: %d \n", guesthouseprice);
+                total_accom = guesthouseprice;
                 break;
                 
             }
-            break;
-        
+            
+            int food;
+            int dayss;
+            int breakfast;
+            int lunch;
+            int dinner;
+            
+            case 3:
+            printf("how many days you are staying \n");
+            scanf("%d",&dayss);
+            printf("details about food expenditure \n");
+            printf("1.Breakfast \t");
+            printf("2.Lunch \t");
+            printf("3.dinner");
+            scanf("%d",&food);
+            
+            switch(food){
+                case 1:
+                printf("enter price for breakfast \n");
+                scanf("%d",&breakfast);
+                printf("breakfast price is %d",breakfast*dayss);
+                total_food = breakfast * dayss;
+                break;
+                
+                case 2:
+                printf("enter price for lunch \n");
+                scanf("%d",&lunch);
+                printf("lunch price is %d",lunch*dayss);
+                total_food = lunch * dayss;
+                break;
+                
+                case 3:
+                printf("enter price for dinner \n");
+                scanf("%d",&dinner);
+                printf("dinner is %d",dinner*dayss);
+                total_food = dinner * dayss;
+                break;
+                
+            }
+            case 4:
+            int budget;
+            printf("decision \n");
+            printf("enter your budget for trip \n");
+            scanf("%d",&budget);
+            expected_price = total_transport + total_accom + total_food;
+            printf("expected price for transporation: %d \n",total_transport);
+            printf("expected price for accomodation: %d \n",total_accom);
+            printf("expected price for food: %d \n",total_food);
+            printf("expected price: %d \n",expected_price);
+            
+            if(expected_price>budget){
+                printf("take loan");
+            }
+            
+            else{
+                printf("%s trip is on",destination);
+                
+            }
         
     }
     return 0;
-}
-
-
-
-
