e0f5634ec90392d9a2821180b621c342dc09f4cd
aaditi-singhh <aditisingh6841@gmail.com>
2025-09-22 23:32:54 +0530

    Add files via upload

---
+++ loops/gungame.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,k;
+    int safe_position;
+    printf("Enter the number of persons :");
+    scanf("%d",&n);
+    printf("Enter the position where the person is sitting is killed : ");
+    scanf("%d",&k);
+    for(int i=2;i<=n;i++){
+        safe_position =(safe_position+k)%i;
+    }
+    printf("the safe safe position in the table:%d\n",safe_position);
+    return 0;
+}
